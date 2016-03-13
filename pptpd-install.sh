3926cea47b14cd1bf9ab2494163f5ee30d22d784
hbwh <hbwhzy@gmail.com>
2016-03-13 23:21:07 +0800

    修理默认IP显示为IPv6地址
    icanhazip.com默认返回IPv6地址，要改为ipv4.icanhazip.com

--- pptpd-install.sh
+++ pptpd-install.sh
@@ -2,20 +2,30 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install pptpd iptables
+apt-get -y autoremove
+apt-get clean
 echo "localip 10.121.116.1" >> /etc/pptpd.conf
 echo "remoteip 10.121.116.2-254" >> /etc/pptpd.conf
 echo "ms-dns 8.8.8.8" >> /etc/ppp/options
 echo "ms-dns 8.8.4.4" >> /etc/ppp/options
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
-echo "Pleast enther pptp user name"
-read username
-echo "Pleast enther pptp password"
-read password
+read -p "Pleast enther pptp user name:" username
+read -p "Pleast enther pptp password:" password
 echo "$username pptpd $password *" >> /etc/ppp/chap-secrets
 service pptpd restart
-echo "Pleast enther IP address"
-read ipad
+echo "Getting Public IP address, Please wait a moment..."
+
+# Get Public IP address
+if [[ "$IP" = "" ]]; then
+    IP=$(wget -O - http://ipinfo.io -o /dev/null | grep "ip" | awk -F\" '{print $4}')
+fi
+echo -e "Your main public IP is\t\033[32m$IP\033[0m"
+
+read -p "(Default IP: $IP):" ipad
+if [ "$ipad" = "" ]; then
+    ipad=IP
+fi
 cat > /etc/pptpdfirewall.sh << EOF
 iptables -t nat -A POSTROUTING -s 10.121.116.0/24 -j SNAT --to-source $ipad
 iptables -A FORWARD -s 10.121.116.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
